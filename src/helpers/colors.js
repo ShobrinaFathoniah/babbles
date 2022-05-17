5036f8249952994c5fd856f6aaca2e8c7eb0203e
ShobrinaFathoniah <shobrinaf@gmail.com>
2022-05-17 22:04:38 +0700

    Add dependencies + colors

---
+++ src/helpers/colors.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  black: '#000000',
+  white: '#FFFFFF',
+  green_100: '#B4FF9F',
+  green_500: '#4E944F',
+  yellow_100: '#F9FFA4',
+  yellow_500: '#F9D923',
+  orange_100: '#FFD59E',
+  red_100: '#FFA1A1',
+  red_500: '#EB5353',
+  purple_100: '#827397',
+  purple_500: '#363062',
+  purple_800: '#1A132F',
+  brown_100: '#BE8C63',
+  brown_500: '#733C3C',
+};
